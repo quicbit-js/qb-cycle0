0d1b3f7faf086053950b859c949de733ad2f1cdf
mvoss9000 <mvoss9000@yahoo.com>
2018-10-08 16:15:23 -0400

    implement clean/even factors

--- index.js
+++ index.js
@@ -34,6 +34,7 @@ function cycle0 (a, max_lambda, clean) {
         for (var i=3; i<len && a[i-1] === a[i]; i++) {}
         if (i === len) { return 1 }
     }
+    max_lambda = max_lambda || 300
     return clean ? _cycle0clean(a, max_lambda) : _cycle0(a, max_lambda)
 }
 
@@ -53,32 +54,55 @@ function _cycle0 (a, max_lambda) {
     return lam
 }
 
-function _cycle0clean (a, max_lambda) {
-    var len = a.length
-    var factors = qbfac.factors(len)    // factor other than len and 1
-    if (factors.length === 0) { return len }
-    var lam = factors[0]
-    while (i < len) {
-        factors.forEach(function (lam) {
-            var i = lam + 1
-
-        })
+// return the lowest cycle of a that is a multiple of f, where f is a prime factor of a.lengths
+function min_cycle (fac, a, max_lambda) {
+    if (fac > max_lambda) {
+        return a.length
     }
-    for (var i = 2; i < len;) {
-        if (a[i] === a[i%lam]) {
-            i++
-        } else {
-            i = ++lam
-            if (lam > max_lambda) {
-                return 0
+    var i = fac
+    var len = a.length
+    // m == 1
+    while (i < len && a[i] === a[i%fac]) { i++ }
+    if (i === len) {
+        return fac
+    }
+
+    // we can optimize for i when there are 1 or more cycles of fac (i.e.  i > fac * 2)
+    //                  xx  **                          xx    **
+    //     [ab, ab, ab, ab, ba, ab, ab ]  means [ abab, abab, baab, ab ]  [ abababab, baabab ]
+    //                               ***                       xxx     ***
+    //     [aba, aba, aba, aba, aba, bba ]  means [ abaaba, abaaba, ababba ]  [ abaabaabaaba, ababba ]
+    var lim = len/fac
+    for (var m=2; m < lim; m++) {
+        var lam = m * fac
+        if (lam > max_lambda) {
+            return len
+        }
+        var f = len/lam
+        if (f === ~~f) {
+            i = lam
+            while (i < len && a[i] === a[i%lam]) { i++ }
+            if (i === len) {
+                return lam
             }
         }
     }
-    return lam
+    return len
+}
+
+function _cycle0clean (a, max_lambda) {
+    var factors = qbfac.factors(a.length)    // factor other than len and 1
+    if (factors.length === 0) {
+        return a.length
+    }
+    var ret = a.length
+    for (var i=0, fac = factors[i]; i<factors.length && fac < ret; i++) {
+        ret = Math.min(ret, min_cycle(factors[i], a, max_lambda))
+    }
+    return ret
 }
 
 function reduce(vals, max_lambda) {
-    max_lambda = max_lambda || 300
     if (vals.length > 1) {
         var lambda = cycle0(vals, max_lambda)
         if (lambda !== 0 && lambda < vals.length) {
